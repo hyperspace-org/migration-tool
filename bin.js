eccba8845fc388a17a92799e47e5e514c6befdc2
Andrew Osheroff <andrewosh@gmail.com>
2020-06-23 16:59:43 +0200

    First commit

---
+++ bin.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const migrate = require('.')
+
+;(async () => {
+  console.log('Migrating from the Hyperdrive daemon to Hyperspace...')
+  try {
+    await migrate()
+    console.log('Migration succeeded!')
+  } catch (err) {
+    console.error('Migration failed:', err)
+  }
+})()
+
---
+++ index.js
@@ -0,0 +1,102 @@
+const os = require('os')
+const p = require('path')
+const fs = require('fs').promises
+
+const level = require('level')
+const sub = require('subleveldown')
+const collectStream = require('stream-collector')
+const { Client: HyperspaceClient, Server: HyperspaceServer } = require('hyperspace')
+
+const HYPERSPACE_ROOT = p.join(os.homedir(), '.hyperspace')
+const HYPERSPACE_STORAGE_DIR = p.join(HYPERSPACE_ROOT, 'storage')
+
+const DAEMON_ROOT = p.join(os.homedir(), '.hyperdrive')
+const DAEMON_STORAGE_DIR = p.join(DAEMON_ROOT, 'storage')
+const DAEMON_DB_PATH = p.join(DAEMON_STORAGE_DIR, 'db')
+const DAEMON_CORES_PATH = p.join(DAEMON_STORAGE_DIR, 'cores')
+
+const MIGRATION_DIR = p.join(DAEMON_STORAGE_DIR, '.migration')
+
+module.exports = async function migrate () {
+  // If the hyperdrive-daemon was never installed, abort.
+  if (await noDaemonInstallation()) return
+
+  // If the hyperspace storage directory has already been created, abort.
+  if (await alreadyMigrated()) return
+
+  const rootDb = level(DAEMON_DB_PATH)
+  const drivesDb = sub(rootDb, 'drives')
+  const networkDb = sub(drivesDb, 'seeding', { valueEncoding: 'json' })
+  await networkDb.open()
+
+  // Move the old storage directory into the migration directory.
+  await migrateCores()
+
+  // Start the Hyperspace server on the migration directory.
+  const server = new HyperspaceServer({
+    storage: MIGRATION_DIR
+  })
+  await server.open()
+  const client = new HyperspaceClient()
+  await client.ready()
+
+  // Migrate the network configurations in the old database into the Hyperspace storage trie.
+  await migrateDatabase(client, networkDb)
+  console.log('Done migrating database')
+
+  // Atomically rename the migration directory to .hyperspace.
+  await fs.mkdir(HYPERSPACE_ROOT, { recursive: true })
+  await fs.rename(MIGRATION_DIR, HYPERSPACE_STORAGE_DIR)
+
+  console.log('Done with migration. Shutting down Hyperspace server...')
+
+  // Shut down the Hyperspace server.
+  await server.close()
+  console.log('Server closed.')
+
+}
+
+async function migrateDatabase (client, db) {
+  const allNetworkConfigs = await dbCollect(db)
+  for (const { key: discoveryKey, value: networkOpts } of allNetworkConfigs) {
+    if (!networkOpts) continue
+    const opts = networkOpts.opts
+    console.log('Migrating discovery key:', discoveryKey)
+    await client.network.configure(Buffer.from(discoveryKey, 'hex'), {
+      announce: !!opts.announce,
+      lookup: !!opts.lookup,
+      remember: true
+    })
+  }
+}
+
+async function migrateCores () {
+  return fs.rename(DAEMON_CORES_PATH, MIGRATION_DIR)
+}
+
+async function noDaemonInstallation () {
+  try {
+    await fs.access(DAEMON_DB_PATH)
+    return false
+  } catch (err) {
+    return true
+  }
+}
+
+async function alreadyMigrated () {
+  try {
+    await fs.access(HYPERSPACE_STORAGE_DIR)
+    return true
+  }  catch (err) {
+    return false
+  }
+}
+
+function dbCollect (index, opts) {
+  return new Promise((resolve, reject) => {
+    collectStream(index.createReadStream(opts), (err, list) => {
+      if (err) return reject(err)
+      return resolve(list)
+    })
+  })
+}
